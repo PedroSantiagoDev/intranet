494158edea5cbf4f4a85fbfc8ae21173db0ca7fb
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-04-10 16:58:38 -0300

    wip

--- resources/views/components/square-card.blade.php
+++ resources/views/components/square-card.blade.php
@@ -6,9 +6,9 @@
 ])
 
 <a href="{{ $url }}" target="{{ $target }}" class="block">
-    <div class="p-4 rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-sm transition-all duration-200 hover:bg-gray-100 dark:hover:bg-gray-300">
+    <div class="p-4 rounded-lg border border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-950 shadow-sm transition-all duration-200 hover:bg-gray-50 dark:hover:bg-white/5">
         <div class="flex gap-2 items-center">
-            @svg($icon, 'w-6 h-6 text-primary-500')
+            @svg($icon, 'w-6 h-6')
             <span class="font-medium">{{ $title }}</span>
         </div>
     </div>
