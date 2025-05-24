aafefe0c5172c712086e1c27cb6929449fc973fb
Pedro Santiago <joaopedrosantiago1103@gamil.com>
2025-05-24 19:35:48 -0300

    user resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\{Pages};
+use App\Models\User;
+use Filament\Forms\Components\{Section, Select, TextInput};
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Filament\{Tables};
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    protected static ?string $label = "Usuários";
+
+    protected static ?string $navigationGroup = 'Configurações';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Informações do Usuário')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('email')
+                            ->label('E-mail')
+                            ->email()
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->maxLength(255),
+                        Select::make('unit_id')
+                            ->label('Unidade')
+                            ->relationship('unit', 'name')
+                            ->required()
+                            ->searchable()
+                            ->preload(),
+                        TextInput::make('password')
+                            ->label('Senha')
+                            ->password()
+                            ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                            ->dehydrated(fn ($state) => filled($state))
+                            ->required(fn (string $context): bool => $context === 'create')
+                            ->maxLength(255)
+                            ->confirmed(),
+                        TextInput::make('password_confirmation')
+                            ->label('Confirmar Senha')
+                            ->password()
+                            ->requiredWith('password')
+                            ->maxLength(255),
+                    ])->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                   ->label('Nome')
+                   ->searchable()
+                   ->sortable(),
+                TextColumn::make('email')
+                    ->label('E-mail')
+                    ->searchable()
+                   ->sortable(),
+                TextColumn::make('unit.name')
+                    ->label('Unidade')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime('d/m/Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                SelectFilter::make('unit_id')
+                ->label('Filtrar por Unidade')
+                ->relationship('unit', 'name')
+                ->searchable()
+                ->preload(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit'   => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
