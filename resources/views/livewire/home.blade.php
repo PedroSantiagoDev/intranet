0370a0b771f904ffe8b344befc2fd52e4b0265e8
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2025-05-29 17:00:21 -0300

    .

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,8 +3,6 @@
         {{-- Seção de Links da Unidade --}}
         <section>
             <div class="p-2 sm:p-4 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
-                <h2 class="text-xl font-bold text-gray-900 dark:text-gray-100 mb-2">Sistemas</h2>
-
                 @if ($visitorLinksHeader->isEmpty())
                     <div class="flex items-center justify-center h-40 border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-lg bg-gray-50 dark:bg-gray-800">
                         <p class="text-gray-500 dark:text-gray-400 text-center">No momento, sua unidade não possui links ativos. Confira novamente mais tarde.</p>
